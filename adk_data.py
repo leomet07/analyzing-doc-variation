08a8a4c4f9aa7481f52062eb73173e48896c3ca5
leomet07 <leomet07@gmail.com>
2025-09-21 14:04:27 -0400

    filter to shp points df as well

--- adk_data.py
+++ adk_data.py
@@ -10,8 +10,8 @@ adk_df["sampledate"] = pd.to_datetime(adk_df["sampledate"], format=f"%m/%d/%y")
 
 adk_df["Permanent_"] = adk_df["Permanent_"].astype(str)
 
-# Filter data df to only contain lakes that are in shapefile dataset
-adk_shp = geopandas.read_file(
+# Load lake bounds shape file
+bounds_shp = geopandas.read_file(
     os.path.join(
         "adk-lakes-shapefiles",
         "bounds",
@@ -19,12 +19,24 @@ adk_shp = geopandas.read_file(
     )
 )
 
-adk_df = adk_df[adk_df["Permanent_"].isin(adk_shp["Permanent_"])]
+
+# Load centroid shape file
+points_shp = geopandas.read_file(
+    os.path.join(
+        "adk-lakes-shapefiles",
+        "points",
+        "adk-lakes-gt.25km2-200m-points-clean-296.geojson",
+    )
+)
+
+# Filter data df to only contain lakes that are in shapefile dataset
+adk_df = adk_df[adk_df["Permanent_"].isin(bounds_shp["Permanent_"])]
+adk_df = adk_df[adk_df["Permanent_"].isin(points_shp["Permanent_"])]
 
 # unique lakes
 unique_lakes_ids = np.unique(adk_df["Permanent_"])
 
-print("Number of unique lakes: ", len(unique_lakes_ids))
-
-print("\nTen most frequent lakes:")
-print(adk_df["Permanent_"].value_counts().nlargest(5))
+if __name__ == "__main__":
+    print("Number of unique lakes: ", len(unique_lakes_ids))
+    print()
+    print("Ten most frequent lakes:\n", adk_df["Permanent_"].value_counts().nlargest(5))
